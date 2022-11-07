4920a2819831c7fc148fe5944c2a9751f616d6cd
chocobaryummy <117326541+chocobaryummy@users.noreply.github.com>
2022-11-07 19:46:41 +0700

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -2,7 +2,7 @@ import { siteConfig } from './lib/site-config'
 
 export default siteConfig({
   // the site's root Notion page (required)
-  rootNotionPageId: '3ed6053553a04e0698a981797052d7cb',
+  rootNotionPageId: '9c410a2457b64e8794efe7ef49154038',
 
   // It's will be used as the root page when running `yarn dev`. (optional)
   // If you are suffering from the slow loading or compiling speed, set this to a smaller page.
@@ -14,16 +14,16 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'FKYnJYQ',
-  domain: 'nexon-demo.vercel.app',
-  author: 'Feng Kaiyu',
+  name: 'Barik',
+  domain: 'yau.my.id',
+  author: 'Barik',
 
   // open graph metadata (optional)
   description: 'Code for Good.',
 
   // social usernames (optional)
-  twitter: 'FKYnJYQ',
-  github: 'fky2015',
+  twitter: 'bmbarik',
+  github: 'chocobaryummy',
   linkedin: '',
   // newsletter: '#', // optional newsletter URL
   // youtube: '#', // optional youtube channel name or `channel/UCGbXXXXXXXXXXXXXXXXXXXXXX`
@@ -38,7 +38,7 @@ export default siteConfig({
   //
   // To generate the following configs, visit https://giscus.app/.
   giscusGithubConfig: {
-    repo: 'fky2015/nexon',
+    repo: 'chocobaryummy/nexon-test',
     repoId: 'MDEwOlJlcG9zaXRvcnkzNzkxNDQ0NTQ=',
     category: 'Comments',
     categoryId: 'DIC_kwDOFplJBs4COB_h',
